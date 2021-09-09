28aeecd67f219dbd8eb92363003f2541c9bb7652
ibuda <ivan.budanaev@gmail.com>
2021-09-09 21:53:54 +0300

    add output directory

--- projects/kaggle-airbnb/jupyter_to_py.py
+++ projects/kaggle-airbnb/jupyter_to_py.py
@@ -4,15 +4,13 @@ import subprocess
 
 
 def convert_files():
-	files = glob.glob("notebooks/*.ipynb")
-	for i, file in enumerate(files):
-		print(f'processing file {i+1}/{len(files)}:\n{file}')
-		subprocess.run(["jupyter", "nbconvert", "--to", "script", file])
-		py_file = f'{file[:-5]}py'
-		new_path = f"src{py_file[py_file.rfind('/'):]}"
-		os.rename(py_file, new_path)
-	print('converting complete.')
+    files = glob.glob("notebooks/*.ipynb")
+    for i, file in enumerate(files):
+        print(f'processing file {i+1}/{len(files)}:\n{file}')
+        subprocess.run(["jupyter", "nbconvert", "--output-dir", "./src", "--to", "script", file])
+
+        print('converting complete.')
 
 
 if __name__ == '__main__':
-	convert_files()
+    convert_files()
